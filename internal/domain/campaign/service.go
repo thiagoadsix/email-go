8f668530a5c9fee375d4090cc249c0b618762f24
Thiago Andrade Silva <thiago.adsix@gmail.com>
2024-06-28 01:06:02 -0300

    refactor: throw possible errors from Service.Create (campaign)

---
+++ internal/domain/campaign/service.go
@@ -0,0 +1,26 @@
+package campaign
+
+import (
+	"emailn/internal/contract"
+	internalerros "emailn/internal/internal-erros"
+)
+
+type Service struct {
+	Repository Repository
+}
+
+func (s *Service) Create(newCampaign contract.NewCampaign) (string, error) {
+	campaign, err := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+
+	if err != nil {
+		return "", err
+	}
+
+	err = s.Repository.Save(campaign)
+
+	if err != nil {
+		return "", internalerros.ErrInternal
+	}
+
+	return campaign.ID, nil
+}

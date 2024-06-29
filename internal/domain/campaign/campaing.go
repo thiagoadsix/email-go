1a7322e44b230572aa6f2cbbbc753a14523902ad
Thiago Andrade Silva <thiago.adsix@gmail.com>
2024-06-29 02:22:43 -0300

    feat: add new property called status to campaign

--- internal/domain/campaign/campaing.go
+++ internal/domain/campaign/campaing.go
@@ -12,12 +12,21 @@ type Campaign struct {
 	Name      string    `validate:"min=5,max=24"`
 	Content   string    `validate:"min=5,max=1024"`
 	Contacts  []Contact `validate:"min=1,dive"`
+	Status    Status    `validate:"required"`
 	CreatedOn time.Time `validate:"required"`
 }
 
 type Contact struct {
 	Email string `validate:"email"`
 }
+
+type Status string
+
+const (
+	Pending Status = "pending"
+	Started Status = "started"
+	Done    Status = "done"
+)
 
 func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
 	contacts := make([]Contact, len(emails))
@@ -31,6 +40,7 @@ func NewCampaign(name string, content string, emails []string) (*Campaign, error
 		Name:      name,
 		Content:   content,
 		Contacts:  contacts,
+		Status:    Pending,
 		CreatedOn: time.Now(),
 	}
 

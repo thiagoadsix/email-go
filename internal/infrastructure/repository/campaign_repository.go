0d833eadab6f86c99fc0a2d56d8dc3234c8711d7
Thiago Andrade Silva <thiago.adsix@gmail.com>
2024-06-29 14:37:55 -0300

    feat: add find by id into repository implementation

--- internal/infrastructure/repository/campaign_repository.go
+++ internal/infrastructure/repository/campaign_repository.go
@@ -15,3 +15,32 @@ func (cr *CampaignRepository) Save(campaign *campaign.Campaign) error {
 func (cr *CampaignRepository) FindAll() ([]campaign.Campaign, error) {
 	return cr.campaigns, nil
 }
+
+func (cr *CampaignRepository) FindByID(id string) (struct {
+	ID      string
+	Name    string
+	Content string
+	Status  string
+}, error) {
+	for _, c := range cr.campaigns {
+		if c.ID == id {
+			return struct {
+				ID      string
+				Name    string
+				Content string
+				Status  string
+			}{
+				ID:      c.ID,
+				Name:    c.Name,
+				Content: c.Content,
+				Status:  c.Status,
+			}, nil
+		}
+	}
+	return struct {
+		ID      string
+		Name    string
+		Content string
+		Status  string
+	}{}, nil
+}

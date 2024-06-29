d2ffe6f33882ea08e815404fd71f0d25c6152dea
Thiago Andrade Silva <thiago.adsix@gmail.com>
2024-06-29 14:36:38 -0300

    feat: add get by id method into service

--- internal/domain/campaign/service.go
+++ internal/domain/campaign/service.go
@@ -25,12 +25,32 @@ func (s *Service) Create(newCampaign contract.NewCampaign) (string, error) {
 	return campaign.ID, nil
 }
 
-func (s *Service) GetAll() ([]Campaign, error) {
+func (s *Service) GetAll() (*[]Campaign, error) {
 	campaigns, err := s.Repository.FindAll()
 
 	if err != nil {
 		return nil, internalerros.ErrInternal
 	}
 
-	return campaigns, nil
+	return &campaigns, nil
+}
+
+func (s *Service) GetById(id string) (*struct {
+	ID      string
+	Name    string
+	Content string
+	Status  string
+}, error) {
+	campaign, err := s.Repository.FindByID(id)
+
+	if err != nil {
+		return &struct {
+			ID      string
+			Name    string
+			Content string
+			Status  string
+		}{}, internalerros.ErrInternal
+	}
+
+	return &campaign, nil
 }

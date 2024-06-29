797106945e1afff41798bd381da639ec40942023
Thiago Andrade Silva <thiago.adsix@gmail.com>
2024-06-28 23:56:49 -0300

    feat: add render library and setup base server

---
+++ cmd/api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	http.ListenAndServe(":3000", r)
+}

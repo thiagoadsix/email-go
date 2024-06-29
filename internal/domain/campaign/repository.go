7bd4feee0349ad598b589243b53cf7cb02f347a7
Thiago Andrade Silva <thiago.adsix@gmail.com>
2024-06-29 14:36:02 -0300

    feat: add find by id method into repository interface

--- internal/domain/campaign/repository.go
+++ internal/domain/campaign/repository.go
@@ -3,4 +3,10 @@ package campaign
 type Repository interface {
 	Save(campaign *Campaign) error
 	FindAll() ([]Campaign, error)
+	FindByID(id string) (struct {
+		ID      string
+		Name    string
+		Content string
+		Status  string
+	}, error)
 }
